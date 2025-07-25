fef05fefcc5dc09f67b0b1c6aa9c4531e4bc30d1
oleksandr_vienik <alexandrVenik@gmail.com>
2025-07-25 12:48:50 +0300

    update kitty config

---
+++ open-kitty.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+// Required parameters:
+// @raycast.schemaVersion 1
+// @raycast.title Open kitty
+// @raycast.mode compact
+
+// Optional parameters:
+// @raycast.icon 🤖
+
+// Documentation:
+// @raycast.description Just a qiuk way to open terminal
+// @raycast.author venikeee1
+// @raycast.authorURL https://raycast.com/venikeee1
+
+console.log("Hello World!");
